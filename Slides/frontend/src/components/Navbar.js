a59430d854d7dbc907d059a01b164e670216bf2f
amnayelamri <53364294+amnayelamri@users.noreply.github.com>
2025-10-30 07:01:57 +0100

    Update Navbar.js

--- Slides/frontend/src/components/Navbar.js
+++ Slides/frontend/src/components/Navbar.js
@@ -18,7 +18,7 @@ const Navbar = () => {
       <div className="navbar-container">
         <Link to="/" className="navbar-brand">
           <FiBook size={28} />
-          <span>Site du Professeur EL Amri</span>
+          <span> EduSlides </span>
         </Link>
 
         <div className="navbar-menu">
